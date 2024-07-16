8920cde4830e2777a2f7edabab7e2cb86fac9992
kyleg <kylegianlatina@gmail.com>
2024-07-16 11:35:19 +0800

    finished presentation

--- client/obj/Debug/net8.0/client.AssemblyInfo.cs
+++ client/obj/Debug/net8.0/client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7232c8cff9b07a4e72cb292079124bebeefb9174")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce00375e7a5717847bf117f8845c5224b5b2c6a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
